695dc445709f22291fc941db370d338e45a902da
nandhu-44 <nandhu90040@gmail.com>
2025-04-03 18:17:04 +0530

    dynamic route

--- src/app/api/admin/applicants/route.js
+++ src/app/api/admin/applicants/route.js
@@ -5,6 +5,8 @@ import Applicant from '@/lib/models/applicant';
 import { cookies } from 'next/headers';
 import { createDiscordEmbed, sendDiscordWebhook } from '@/lib/sendWebhook';
 
+export const dynamic = 'force-dynamic';
+
 export async function GET(request) {
   try {
     await verifyAuth();
